cc7dd1deb56755252a9cf1d23b1a17f370a52ecd
Dhwani Trivedi <41083525+dhwaniT99@users.noreply.github.com>
2024-03-03 12:53:52 -0500

    Add initial project files

---
+++ ml_ops/calculator.py
@@ -0,0 +1,16 @@
+#Func1(x,y): adds x + y
+#Func2(x,y): Multiplies x, y
+#func3(x,y): Subtracts x and y
+#Func4(x,y): divides x and y 
+
+
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return x - y 
+
+def multiply(x,y):
+    return x*y
+
+
